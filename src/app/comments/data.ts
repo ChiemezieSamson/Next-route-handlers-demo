9aa4a668fcefeb05f279d0b561a23166015d9b42
Nebeolisa chiemezie Samson <daddygreenarrow44@gmail.com>
2024-01-29 23:20:06 +0900

    Made a get request

---
+++ src/app/comments/data.ts
@@ -0,0 +1,14 @@
+export const comment = [
+  {
+    id: 0,
+    text: "This is the first comment"
+  },
+  {
+    id: 0,
+    text: "This is the second comment"
+  },
+  {
+    id: 0,
+    text: "This is the third comment"
+  },
+]
